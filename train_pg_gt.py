try to fix policy gradient

train_pg_gt.py
@@ -9,7 +9,6 @@ class Agent():
         self.lr = lr
         self.gamma = gamma
         self.k = k
-        self.g_t = 0
 
     def set_generator(self, rand_generator):
         self.rand_generator = rand_generator
@@ -19,23 +18,29 @@ class Agent():
         self.optimizer = torch.optim.Adam(self.policy.parameters())
         self.num_actions = num_actions
 
-    def calc_loss(self, s, a, r, s_tag):
-        return -torch.sum(self.lr * torch.log(self.policy(s)) * self.g_t)
+    def calc_loss(self, s, gamma_t, g_t):
+        return -torch.sum(self.lr * gamma_t * g_t * torch.log(self.policy(s)))
 
     def choose(self, s):
         p = self.policy(s).cpu().detach().numpy()
         a = self.rand_generator.choice(list(range(self.num_actions)), p=p)
         return a
 
-    def update(self, state_list, action, new_state, reward_list):
-        self.g_t = 0
-        for state, reward in zip(reversed(state_list), reversed(reward_list)):
-            self.g_t = self.g_t * self.gamma + reward
-        pseudo_loss = self.calc_loss(state, action, reward, new_state)
-        # update policy weights
-        self.optimizer.zero_grad()
-        pseudo_loss.backward()
-        self.optimizer.step()
+    def step(self, state_list, action_list, reward_list):
+        gamma_t = 1
+        for t in range(len(state_list)):
+            g_t = 0
+            for k in reversed(range(t, len(state_list))):
+                g_t = g_t * self.gamma + reward_list[k]
+            state = state_list[t]
+            # action = action_list[t]
+            # reward = reward_list[t]
+            pseudo_loss = self.calc_loss(state, gamma_t, g_t)
+            # update policy weights
+            self.optimizer.zero_grad()
+            pseudo_loss.backward()
+            self.optimizer.step()
+            gamma_t *= self.gamma
 
 
 if __name__ == "__main__":
@@ -44,7 +49,7 @@ if __name__ == "__main__":
     episodes = 500000
     episode_step = 20
     gamma = 0.99
-    lr = 0.01
+    lr = 0.3
     p_array = [0.9, 0.1]
     k = 10
     seed = 42

utils.py
@@ -115,17 +115,19 @@ class Controller:
         # reset environment
         state = self.env.reset()
         for n_episode in range(self.episodes):
-            rewards = []
-            states = []
+            rewards_list = []
+            states_list = []
+            actions_list = []
             for _ in range(self.episode_step):
-                states.append(state)
+                states_list.append(state)
                 action = self.agent.choose(state)
-                new_state, reward = self.env.step(action)
-                state = new_state
-                rewards.append(reward)
-            self.agent.update(states, action, new_state, rewards)
+                state_next, reward = self.env.step(action)
+                state = state_next
+                rewards_list.append(reward)
+                actions_list.append(action)
+            self.agent.step(states_list, actions_list, rewards_list)
             state = self.env.reset()
 
             # calculate average return and print it out
-            self.returns.append(np.sum(rewards))
+            self.returns.append(np.sum(rewards_list))
             print(f"Episode: {n_episode:6d}\tAvg. Return: { np.mean(self.returns):6.2f}")